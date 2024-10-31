603512f4b79150bb41c49db19afc22ea945f5943
Behrooz Payoon <behrooz.payoon@gmail.com>
2024-10-31 20:24:23 +0330

    Fix bg color

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <Html>
     <Body class="bg-gray-100 dark:bg-gray-800">
-      <NuxtLayout>
+      <NuxtLayout class="bg-gray-100 dark:bg-gray-800">
         <NuxtPage />
       </NuxtLayout>
     </Body>
